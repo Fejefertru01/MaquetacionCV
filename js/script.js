3ca58e5566cb2cea0f6adfda0b2707503cc060cc
Fejefertru01 <fejefetru@gmail.com>
2024-01-25 14:41:56 +0100

    Mas avances

--- js/script.js
+++ js/script.js
@@ -2,42 +2,24 @@ $(document).ready(() => {
 
     $('#date').datepicker();
 
-    
+
 
     $('#oscuro').click(() => {
-        // oscuro
-        if ($(':root').css('--color-principal') == '#FFFFFF') {
-            $(':root').css('--color-principal', '#1a1110');
-            $(':root').css('--color-secondary', '#a9a9a9');
-            $(':root').css('--color-title', '#FFFFFF');
-            $(':root').css('--color-subtitle', '#FFFFFF');
-            $(':root').css('--color-text', '#1a1110');
-            $(':root').css('--color-background', 'lightgrey');
-            $('.oscuro').addClass('fa-regular fa-sun').removeClass('fa-solid fa-moon');
-            $('#cambiarSpan').text('Modo Claro');
-            // claro
-        } else {
-            $(':root').css('--color-principal', '#FFFFFF');
-            $(':root').css('--color-secondary', '#77b95e');
-            $(':root').css('--color-title', '#565656');
-            $(':root').css('--color-subtitle', '#888');
-            $(':root').css('--color-text', '#555');
-            $(':root').css('--color-background', '#FFFFFF');
-            $('.oscuro').addClass('fa-solid fa-moon').removeClass('fa-regular fa-sun');
-            $('#cambiarSpan').text('Modo Oscuro');
-
-        }
+        sessionStorage.setItem('modoNoche', !$(':root').hasClass('modo_oscuro'));
+        cambiarTema();
     });
+    cambiarTema();
+    
 
-    $('#volverArriba').on('click', function() {
+    $('#volverArriba').on('click', function () {
         $('.content__page').animate({
             scrollTop: 0
         }, 600);
         return false;
     });
-    
 
-    
+
+
 
     $('.layout').hide(400, () => {
         $('.layout').show(400);
@@ -49,13 +31,24 @@ $(document).ready(() => {
 
     $('.certificates__certificate').click(() => {
         $('#popup').dialog();
-      });
-    
+    });
+
     $('.user-infon__ame').slideUp(600, () => {
         $('.user-info__name').slideDown(600);
-        });
-    
+    });
+
     $('.user-info__job').slideUp(600, () => {
         $('.user-info__job').slideDown(600);
-        });
+    });
 });
+const cambiarTema = () => {
+        if (!sessionStorage.getItem('modoNoche') || sessionStorage.getItem('modoNoche') === 'false') {
+            $(':root').removeClass('modo_oscuro');
+            $('#oscuro i').addClass('fa-moon').removeClass('fa-sun');
+            $('#oscuro span').text('Modo Oscuro');
+        } else {
+            $(':root').addClass('modo_oscuro');
+            $('#oscuro i').addClass('fa-sun').removeClass('fa-moon');
+            $('#oscuro span').text('Modo Claro');
+        }
+    }
